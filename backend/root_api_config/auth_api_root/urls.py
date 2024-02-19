60f86c98b8602fa65b02c11114277d370fe5a3d9
Trash Pitch <workumyfather@gmail.com>
2024-02-19 22:31:24 +0300

    new backend setup

---
+++ backend/root_api_config/auth_api_root/urls.py
@@ -0,0 +1,29 @@
+# django imports
+from django.urls import path
+
+#3rd party
+from drf_social_oauth2.urls import TokenView
+from drf_social_oauth2.urls import RevokeTokenView
+
+# project view applications
+from .views import (
+    UserSignUpAPIView,
+    UserInformationRetrieveAPIView,
+    UserProfileUpdateAPIView,
+    get_media_path,
+)
+
+
+urlpatterns = [
+    # basic login, sign in and logout pages
+    path('signup/', UserSignUpAPIView.as_view(), name='signup'),
+    path('login/' , TokenView.as_view() , name='login'),
+
+    # update profile
+    path('profile/update/<str:username>/', UserProfileUpdateAPIView.as_view(), name='profile_update_by_serializer'),
+
+    # dump user information if needed
+    path('profile/<str:username>/',  UserInformationRetrieveAPIView.as_view(), name='profile_info'),
+    path("media/<str:path>", get_media_path, name="get-media-path"),
+    
+]
---
+++ backend/root_api_config/auth_api_root/views.py
@@ -0,0 +1,91 @@
+import mimetypes
+import os
+from urllib.parse import unquote
+
+
+from django.conf import settings
+from django.http import FileResponse
+
+
+# rest frame work imports
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
+
+
+# project imports
+from .models import AuthUserModel as User
+from .serializer import UserSerializer, RegisterSerializer
+
+#-------------------class based views-------------------------
+
+class UserSignUpAPIView(generics.CreateAPIView):
+    """
+    Basic api end point to register a  user
+    """
+    queryset = User
+    serializer_class = RegisterSerializer
+    
+
+class UserInformationRetrieveAPIView(generics.RetrieveAPIView):
+    """
+    Basic end point to find a user by user name and send the user back
+    """
+    lookup_field = 'username'
+    queryset = User
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+
+class UserProfileUpdateAPIView(generics.UpdateAPIView):
+    """
+    Api end point to update user after searching it with it's username
+
+    required field include
+
+        username: str
+        password: str
+        email: str
+    
+    other fields are not required
+    """
+    lookup_field = 'username'
+    queryset = User
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+#-------------------- function based views -----------------------
+    
+
+@api_view(["GET"])
+# @permission_classes([IsAuthenticated])
+def get_media_path(request, path) -> FileResponse:
+    """
+    The get_media_path function is a helper function that takes in the request and
+    path of a file. 
+    
+    It then checks if the file exists, and returns an error message if
+    it does not exist. 
+    
+    If it does exist, it will return an HttpResponse with the
+    correct headers to serve up the media.
+    
+    @param path: Determine the path of the file to be served
+    """
+    
+    if not os.path.exists(f"{settings.MEDIA_ROOT}/music_files/audio/{path}"):
+        return Response("No such file exists.", status=404)
+
+    # Guess the MIME type of a file. Like pdf/docx/xlsx/png/jpeg
+    mimetype, encoding = mimetypes.guess_type(f"{settings.MEDIA_ROOT}/music_files/audio/{path}", strict=True)
+    if not mimetype:
+        mimetype = "text/html"
+
+    # By default, percent-encoded sequences are decoded with UTF-8, and invalid
+    # sequences are replaced by a placeholder character.
+        
+    file_path = unquote(os.path.join(f"{settings.MEDIA_ROOT}/music_files/audio/{path}")).encode("utf-8")
+    
+    return FileResponse(open(file_path, "rb").read(), content_type=mimetype)
