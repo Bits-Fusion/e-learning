4df3e2754b776b9d0e9fe91e184947c2e482f09e
Trash Pitch <workumyfather@gmail.com>
2024-03-22 22:04:29 +0300

    Authorization and error handeling

--- backend/nest_app/src/video-stream/video-stream.controller.ts
+++ backend/nest_app/src/video-stream/video-stream.controller.ts
@@ -20,15 +20,34 @@ export const storage = {
 @Controller('video')
 export class VideoStreamController {
     @Get(':__path')
-    videoStream (@Param('__path') __path: string, @Headers() header: any, @Res() res: any) {
+    async videoStream (@Param('__path') __path: string, @Headers() header: any, @Res() res: any) {
         const range = header.range
-        if(!range){
-            return res.status(404).send({'detail':'The header do not contain range'})
+        const authHandler = await handelAuth(header);
+
+        if(!authHandler) {
+            return res.status(500).send({
+                'detail':'There has been internal server error'
+            })
         }
+
+        if(authHandler.error) {
+            return res.status(401).send({
+                'detail':'invalid creadential'
+            })
+        }
+
+        if(!range){
+            return res.status(404).send({
+                'detail':'The header do not contain range parameter'
+            })
+        }
+
         const videoPath = path.resolve(__dirname, "../../../", `media/music_files/audio/${__path}`);
 
         if(!fs.existsSync(videoPath)) {
-            return res.status(404).send({"detail":"The file you are looking for doesn't exist in the database"});
+            return res.status(404).send({
+                "detail":"The file you are looking for doesn't exist in the database"
+            });
         }
 
         const videoSize = fs.statSync(videoPath).size;
@@ -36,9 +55,12 @@ export class VideoStreamController {
         const _splited = range.replace(/\D/g, ' ').split();
         const start = Number(_splited[0]);
         const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+        
 
         if(start >= videoSize){
-            return res.status(404).send({"detail":"Out of range"});
+            return res.status(404).send({
+                "detail":"Out of range"
+            });
         }
         
         const contentLength = end - start + 1;
@@ -56,6 +78,48 @@ export class VideoStreamController {
             });
 
         videoStream.pipe(res);
-        return path
+        return path;
     }
+}
+
+const handelAuth = async (header: any) => {
+    const containToken: string = await header.authorization;
+
+    if (!containToken){
+        return {
+            'detail' : 'The Request does not contain Authorization token',
+            'error' : true,
+        }
+    }
+    const token = containToken.split(' ')
+
+    const BASE_URL = 'http://localhost:9100/api_root/auth/validate/';
+
+    const Options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            'token': token[1],
+        })
+    };
+
+    try {
+        const message = await fetch(BASE_URL, Options)
+        .then((response) => {
+            return response.json();
+        })
+        .catch(error => {
+            console.log('error', error);
+        })
+        return message
+
+    } catch (error) {
+        return {
+            'detail': error,
+            'error': true,
+        }
+    }
+
 }
